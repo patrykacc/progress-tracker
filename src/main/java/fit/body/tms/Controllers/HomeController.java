259976ae849b5c15dcedf4f08e7aaa848b82fdca
Patryk Organa <organapatryk@gmail.com>
2019-10-13 19:03:37 +0200

    production build test

---
+++ src/main/java/fit/body/tms/Controllers/HomeController.java
@@ -0,0 +1,14 @@
+package fit.body.tms.Controllers;
+
+public class ViewController {
+
+/*    @RequestMapping("/")
+    public String redirectHome() {
+        return "index";
+    }
+
+    @RequestMapping("/api")
+    public String redirectHomeApi() {
+        return "index";
+    }*/
+}
