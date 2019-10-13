b08520e6fda09fb580dc68fb82b8a18b00819a20
Patryk Organa <organapatryk@gmail.com>
2019-10-13 21:46:07 +0200

    test

--- src/main/java/fit/body/tms/Controllers/HomeController.java
+++ src/main/java/fit/body/tms/Controllers/HomeController.java
@@ -1,14 +1,13 @@
 package fit.body.tms.Controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
+    /*@RequestMapping(value = "/")
     public String index() {
         return "index";
-    }
+    }*/
 
 }
